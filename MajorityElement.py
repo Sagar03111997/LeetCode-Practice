944ed72c460d1a215262399323c7b2c70c16deb0
Sagar <sdsahu@iu.edu>
2022-11-02 00:00:42 -0400

    changes in code

--- MajorityElement.py
+++ MajorityElement.py
@@ -1,5 +1,13 @@
 def majorityElement(nums):
-        
+    dict = {}
+
+    for num in nums:
+        if num not in dict:
+            dict[num] = 1
+        else:
+            dict[num] += 1
+    max_value = max(dict, key = dict.get)
+    return max_value
 
 
 print(majorityElement([2,2,1,1,1,2,2,1,1,1,1]))
